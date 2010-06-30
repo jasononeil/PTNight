8f630aa82a1f044654f08722695de1d1a7d1ebe0
Jason O'Neil <jason.oneil@gmail.com>
2010-06-30 15:50:52 +0800

    Inserted code for HxTpl and my Xml classes (XmlNode, XmlList)
    Imported from other projects.

---
+++ php/lib/hxbase/BaseController.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class hxbase_BaseController {
+	public function __construct($args) {
+		;
+		;
+	}
+	public $isCacheable;
+	public $output;
+	public function toString() {
+		return $this->output;
+	}
+	public function hprint($str) {
+		$this->output = $this->output . Std::string($str);
+	}
+	public function clearOutput() {
+		$this->output = "";
+	}
+	public function __call($m, $a) {
+		if(isset($this->$m) && is_callable($this->$m))
+			return call_user_func_array($this->$m, $a);
+		else if(isset($this->»dynamics[$m]) && is_callable($this->»dynamics[$m]))
+			return call_user_func_array($this->»dynamics[$m], $a);
+		else if('toString' == $m)
+			return $this->__toString();
+		else
+			throw new HException('Unable to call «'.$m.'»');
+	}
+	function __toString() { return $this->toString(); }
+}
