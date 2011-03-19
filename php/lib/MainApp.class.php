1dae796bdcaa014a9e222e60eca9b5163d99647d
Jason O'Neil <jason.oneil@gmail.com>
2011-03-20 03:06:13 +0800

    Added the "basehx.App" class, to remove more stuff from the MainApp.hx file.

--- php/lib/MainApp.class.php
+++ php/lib/MainApp.class.php
@@ -2,31 +2,14 @@
 
 class MainApp {
 	public function __construct(){}
-	static $startTime;
 	static function main() {
-		haxe_Log::$trace = (isset(basehx_Log::$trace) ? basehx_Log::$trace: array("basehx_Log", "trace"));
-		haxe_Log::trace("FATTY", _hx_anonymous(array("fileName" => "MainApp.hx", "lineNumber" => 14, "className" => "MainApp", "methodName" => "main")));
-		php_Lib::hprint("<pre>");
-		$request = php_Web::getParams()->get("request");
-		basehx_Dispatcher::dispatch($request);
-		php_Lib::hprint("</pre>");
-		MainApp::printStats();
+		basehx_App::initiate();
 	}
-	static $pageTemplateFile;
+	static $pageTemplateFile = "views/MainView.tpl";
 	static function initiatePageTemplate() {
 		$template = new basehx_tpl_HxTpl();
 		$template->loadTemplateFromFile(MainApp::$pageTemplateFile);
 		return $template;
 	}
-	static function printStats() {
-		php_Lib::hprint("<pre>");
-		$memory = memory_get_peak_usage();
-		$memory = intval($memory / 1000);
-		haxe_Log::trace(("Memory usage: " . $memory) . "kb", _hx_anonymous(array("fileName" => "MainApp.hx", "lineNumber" => 44, "className" => "MainApp", "methodName" => "printStats")));
-		$executionTime = php_Sys::time() - MainApp::$startTime;
-		haxe_Log::trace("Execution Time: " . $executionTime, _hx_anonymous(array("fileName" => "MainApp.hx", "lineNumber" => 47, "className" => "MainApp", "methodName" => "printStats")));
-		php_Lib::hprint("</pre>");
-	}
 	function __toString() { return 'MainApp'; }
 }
-MainApp::$startTime = php_Sys::time();
--- src/MainApp.hx
+++ src/MainApp.hx
@@ -1,50 +1,18 @@
-import basehx.Dispatcher;
-import basehx.DbControl;
-import AppConfig;
-import basehx.Log;
+import basehx.App;
 import basehx.tpl.HxTpl;
-
-class MainApp
+class MainApp 
 {
-	public static var startTime:Float = php.Sys.time();
-	
-	public static function main()
+	static public function main() 
 	{
-		haxe.Log.trace = basehx.Log.trace;
-		trace ("FATTY");
-		php.Lib.print('<pre>');
-		
-		// Pass control off to the dispatcher, 
-		// (which will find the appropriate Controller)
-		var request:String = php.Web.getParams().get("request");
-		Dispatcher.dispatch(request);
-		
-		php.Lib.print('</pre>');
-		printStats();
+		App.initiate();
 	}
 	
-	/** This allows you to set a site-wide HTML template for your App.
-	You can also set a template for each controller, but if that is not
-	there it will full back to this.  Should this stuff be kept in
-	AppConfig instead?    */
-	public static var pageTemplateFile(default,null):String;
-	public static function initiatePageTemplate():HxTpl
+	static public var pageTemplateFile = "views/MainView.tpl";
+	
+	static public function initiatePageTemplate() 
 	{
 		var template = new HxTpl();
 		template.loadTemplateFromFile(pageTemplateFile);
 		return template;
 	}
-	
-	public static function printStats()
-	{
-		php.Lib.print('<pre>');
-		#if php
-		var memory:Int = untyped __call__("memory_get_peak_usage");
-		memory = Std.int(memory / 1000);
-		trace ("Memory usage: " + memory + "kb");
-		#end
-		var executionTime:Float = php.Sys.time() - startTime;
-		trace ("Execution Time: " + executionTime);
-		php.Lib.print('</pre>');
-	}
-} 
+}
